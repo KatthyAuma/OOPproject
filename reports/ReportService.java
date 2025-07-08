de62693274eb729b22e02c9ad49d12277a1ebe4a
elavaza16 <elavazasandra73@gmail.com>
2025-07-08 09:47:58 +0300

    Delete reports directory

--- reports/ReportService.java
+++
@@ -1,9 +0,0 @@
-package com.example.oop_systems_project.reports;
-
-public interface ReportService {
-    String CustomerwithOrdersReport();
-    String OrdersByBranchReport();
-    String SalesByBranchReport();
-    String LowStockReport(int threshold);
-    String TotalSalesReport();
-}
